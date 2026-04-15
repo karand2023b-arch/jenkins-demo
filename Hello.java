b0d742c1ae63209542298eca9462328a2d1e5724
karand2023b-arch <karan.d2023a@vitstudent.ac.in>
2026-04-15 17:44:24 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,3 +3,19 @@ public class Hello {
         System.out.println("Hello from CI Project!");
     }
 }
+Smart Electricity Billing System
+Problem Statement:
+Ensure electricity usage is calculated and billed correctly.
+Tasks:
+1. Develop billing calculation logic
+2. Write JUnit tests for tariff calculations
+3. Implement CI/CD pipeline with automated testing
+➢ Set up version control using GitHub
+➢ Configure CI pipeline (e.g., Jenkins/GitHub Actions) to trigger on code commits
+➢ Automate build process using Maven
+➢ Integrate automated unit tests in pipeline
+➢ Configure CD pipeline for automatic deployment to staging/production
+4. Deploy services on Docker and Kubernetes
+➢ Containerize application using Docker (write Dockerfile)
+➢ Build and push Docker image to container registry (Docker Hub)
+➢ Create Kubernetes deployment and service YAML files
